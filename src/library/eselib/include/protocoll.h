275d0a401e5173ea90fc4be7199762ba3540d311
igor.pelesic@gmail.com <igor.pelesic@gmail.com>
2012-12-01 16:23:59 +0100

    protocol improvement

--- src/library/eselib/include/protocoll.h
+++ src/library/eselib/include/protocoll.h
@@ -48,9 +48,10 @@
 #define SET_BUS_HIGH (HW0_PORT |= (1<<HW0_TX))
 #define SET_BUS_LOW  (HW0_PORT &= ~(1<<HW0_TX))
 
-#define  PROTCL_MAX_MESSAGE            15
-# define BAUD                57600
-# define MODE_8E1            0x26       /*8 bits, even parity, 1 stop bit*/
+#define PROTCL_MAX_MESSAGE            15
+#define BAUD                57600
+#define BAUD1				115200
+#define MODE_8E1            0x26       /*8 bits, even parity, 1 stop bit*/
 
 
 
