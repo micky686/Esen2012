81ada993823fa01f795764ea3d3a1944e2e49e73
selyunin.k.v <selyunin.k.v@gmail.com>
2012-12-13 15:32:54 +0100

    asm for agent v0.1

---
+++ src/app/asm/asm.py
@@ -0,0 +1,180 @@
+import re
+import sys
+
+
+LABEL = r"[A-Za-z][A-Za-z0-9]{0,5}"
+MNEMONIC = r"add|sub|div|mul|jmpgr|jmpeq|jmpls|compare|move|clone|die|sendmsg|pullmsg|store|storecr|mv|wait|priority|setservice|getservice"
+REG = r"acc|reg_0|reg_1|reg_2|reg_3|reg_4|reg_5|reg_6|reg_str_0|reg_str_1|reg_str_2"
+VALUE = r"0x[0-9A-F]{1,2}|0b[01]{1,8}|[0-9]{1,3}"
+OPERAND = REG + "|" + VALUE 
+LEGAL_LABEL = "((?!" + MNEMONIC + "|" + OPERAND + ")" + LABEL +")"
+LABEL_LOOKUP = "(?:" + LEGAL_LABEL + "\s*:)?" + "\s*(" + MNEMONIC + ")" + "(?:\s+" + LEGAL_LABEL + ")?"
+OPERAND_LOOKUP = "(?:" + LEGAL_LABEL + "\s*:)?" + "\s*(" + MNEMONIC + ")" + "(?:\s+(" + OPERAND + "))?\s*(?:,\s*(" + OPERAND + "))?\s*(?:,\s*(" + OPERAND + "))?\s*"
+
+reg = {"reg_0":"0000",\
+           "reg_1":"0001",\
+           "reg_2":"0010",\
+           "reg_3":"0011",\
+           "reg_4":"0100",\
+           "reg_5":"0101",\
+           "reg_6":"0110",\
+           "acc"  :"1111"}
+
+reg_str = {"reg_str_0":"1000",\
+              "reg_str_1":"1001",\
+              "reg_str_2":"1010"}
+
+command_opcode = {"add":"00000011",\
+               "addv":"0011",\
+               "sub":"00000110",\
+               "subv":"0110",\
+               "div":"00001001",\
+               "divv":"1001",\
+               "mul":"00001100",\
+               "mulv":"1100",\
+               "jmpgr":"11110011",\
+               "jmpeq":"11110110",\
+               "jmpls":"11111100",\
+               "compare":"00001010",\
+               "comparev":"1010",\
+               "move":"11110001",\
+               "clone":"1111001000000000",\
+               "die":"1111010000000000",\
+               "sendmsg":"11111000",\
+               "pullmsg":"111110100000",\
+               "store":"1101",\
+               "storecr":"1011",\
+               "mv":"00001101",\
+               "wait":"00000101",\
+               "priority":"00001000",\
+               "setservice":"0111",\
+               "getservice":"00000111"}
+
+command = {\
+        "add":{"OPERAND1":reg, "OPERAND2":[reg, "value"], "OPERAND3":None, "LABEL2":None},\
+        "sub":{"OPERAND1":reg, "OPERAND2":[reg, "value"], "OPERAND3":None, "LABEL2":None},\
+            }
+               
+#LEGAL LINE:
+#LABEL:? MNEMONIC (?: OPERAND1? (?:, OPERAND2?)) | (LABEL2)
+FILENAME = r""
+SOURCE_FILE = r""
+FIRST_PARS = {}
+
+def asm_tool(filename):
+    if check_files(filename) == None:
+        return None
+    first_pass()
+    second_pass()
+
+
+def first_pass():
+    source = open(SOURCE_FILE , "r")
+    num_lines = sum(1 for line in open(SOURCE_FILE))
+    i,k=1,0
+    while i <= num_lines:
+        source_line = source.readline()
+        label_lookup = re.compile(LABEL_LOOKUP).match(source_line)
+        operand_lookup = re.compile(OPERAND_LOOKUP).match(source_line)
+        if label_lookup != None:
+            label_1, label_2 = label_lookup.group(1), label_lookup.group(3)
+            mnemonic = label_lookup.group(2)
+            operand_1, operand_2, operand_3 = operand_lookup.group(3), operand_lookup.group(4), operand_lookup.group(5)
+            FIRST_PARS[k] = {"LABEL1":label_1,"MNEMONIC":mnemonic,"LABEL2":label_2,\
+                                 "OPERAND1":operand_1, "OPERAND2":operand_2, "OPERAND3":operand_3, "LINE":i}
+            k+=1
+        i+=1
+    source.close()
+    
+def second_pass():
+#    print FIRST_PARS
+    binary = open(FILENAME + ".bin", "w+")
+    listing = open(FILENAME + ".lst", "w+")
+    i = 0
+
+    while i < len(FIRST_PARS):
+        #assemble line
+        opcode = ""
+        mnem = FIRST_PARS[i]["MNEMONIC"].lower()
+        if  mnem == "add" or mnem == "sub" or mnem == "mul" or mnem == "div" or mnem == "compare":
+           if FIRST_PARS[i]["LABEL2"] != None:
+               print "Error in line {}, illegal label ".format(FIRST_PARS[i]["LINE"])
+           elif  FIRST_PARS[i]["OPERAND1"] not in reg or FIRST_PARS[i]["OPERAND2"] == None or FIRST_PARS[i]["OPERAND3"] != None:
+               print "Error in line {}, illegal operand".format(FIRST_PARS[i]["LINE"])
+           elif FIRST_PARS[i]["OPERAND2"] in reg:
+               opcode = command_opcode[mnem] + reg[FIRST_PARS[i]["OPERAND1"]]+reg[FIRST_PARS[i]["OPERAND2"]]
+           elif re.compile(VALUE).match(FIRST_PARS[i]["OPERAND2"]) != None:
+               opcode = command_opcode[mnem + "v"] + reg[FIRST_PARS[i]["OPERAND1"]] + convert_to_binary(FIRST_PARS[i]["OPERAND2"])
+        if mnem == "jmpeq" or mnem == "jmpls" or mnem == "jmpgr":
+            if FIRST_PARS[i]["OPERAND1"] != None or FIRST_PARS[i]["OPERAND2"] != None or FIRST_PARS[i]["OPERAND3"] != None:
+                print "Error in line {}, illegal operand".format(FIRST_PARS[i]["LINE"])
+            elif FIRST_PARS[i]["LABEL2"] == None:
+                print "Error in line {}, illegal label".format(FIRST_PARS[i]["LINE"])
+            else:
+            #find labels to generate offset!!!
+                offset = 1#dummy
+                offset = convert_to_binary(offset)
+                opcode = command_opcode[mnem] + offset
+        if mnem == "clone" or mnem == "die":
+            if FIRST_PARS[i]["OPERAND1"] != None or FIRST_PARS[i]["OPERAND2"] != None or FIRST_PARS[i]["OPERAND3"] != None:
+                print "Error in line {}, illegal operand".format(FIRST_PARS[i]["LINE"])
+            elif FIRST_PARS[i]["LABEL2"] != None:
+                print "Error in line {}, illegal label ".format(FIRST_PARS[i]["LINE"])
+            else:
+                opcode = command_opcode[mnem]
+        if mnem == "mv":
+            if FIRST_PARS[i]["LABEL2"] != None:
+               print "Error in line {}, illegal label ".format(FIRST_PARS[i]["LINE"])
+            elif  FIRST_PARS[i]["OPERAND1"] not in reg or FIRST_PARS[i]["OPERAND2"] not in reg or FIRST_PARS[i]["OPERAND3"] != None:
+               print "Error in line {}, illegal operand".format(FIRST_PARS[i]["LINE"])
+            else:
+                opcode = command_opcode[mnem] + reg[FIRST_PARS[i]["OPERAND1"]] + reg[FIRST_PARS[i]["OPERAND2"]]
+        print FIRST_PARS[i]
+        print opcode
+        binary.write(opcode)
+        listing.write(xstr(FIRST_PARS[i]["LABEL1"]) + "\t\t" + opcode + "\t\t" + xstr(FIRST_PARS[i]["MNEMONIC"]) + "\t\t" + \
+                         xstr(FIRST_PARS[i]["OPERAND1"]) + "\t\t" + xstr(FIRST_PARS[i]["OPERAND2"]) + "\t\t" + \
+                          xstr(FIRST_PARS[i]["OPERAND3"]) + "\t\t" + xstr(FIRST_PARS[i]["LABEL2"]) + "\n")
+        i+=1
+
+       # if FIRST_PARS[i]["OPERAND1"] in command[FIRST_PARS[i]["MNEMONIC"]]["OPERAND1"]:
+       #     opcode = opcode + command[FIRST_PARS[i]["MNEMONIC"]]["OPERAND1"][FIRST_PARS[0]["OPERAND1"]]
+       # binary.write(opcode+"\n")
+
+def convert_to_binary(value):
+    bin_value = ""
+    if re.compile("^0x([0-9a-fA-F]{1,2})$").match(value) != None:
+          bin_value = bin(int(re.compile("^0x([0-9a-fA-F]{1,2})$").match(value).group(1), 16))[2:]
+    elif re.compile("^0b([01]{1,8})$").match(value) != None:
+          bin_value = re.compile("^0b([01]{1,8})$").match(value).group(1)
+    elif re.compile("^[+-]?([01]?[0-9]?[0-9])$").match(value) != None:
+          #negative numbers
+          bin_value = bin(int(re.compile("^[+-]?([01]?[0-9]?[0-9])$").match(value).group(1)))[2:]
+    else:
+          print "invalid value"
+          return None
+    while len(bin_value) < 8:
+          bin_value = "0" + bin_value
+    return bin_value
+       
+
+def check_files(filename):
+    """check agent file for .ma extension, extract filename for listing, hex, binary generation"""
+    SOURCE_REGEX = re.compile("([A-Za-z0-9_]+)\.ma").match(filename)
+    if SOURCE_REGEX == None:
+        print "\tInvalid file!\n\tSpecify agent file with extension .ma\t!\n"
+        return None
+    global SOURCE_FILE, FILENAME
+    SOURCE_FILE = SOURCE_REGEX.group(0)
+    FILENAME = SOURCE_REGEX.group(1)
+    print SOURCE_FILE + " to be assembled\n"
+    return SOURCE_FILE
+
+def xstr(s):
+    if s == None:
+        return ""
+    else:
+        return s
+
+asm_tool(sys.argv[1])
+
